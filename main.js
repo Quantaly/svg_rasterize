b5e6a73d0824c45c8e8e5e1b6a067624c8bc0704
Kai Page <kaibug@gmail.com>
2019-10-28 17:46:02 -0500

    Select multiple files

--- main.js
+++ main.js
@@ -10,25 +10,26 @@
     }
 
     svgImport.addEventListener("input", _ => {
-        //console.log(changeExtension(svgImport.files[0].name, "png"));
-        const file = svgImport.files[0];
-        const image = document.createElement("img");
-        image.addEventListener("load", _ => {
-            const canvas = document.createElement("canvas");
-            canvas.width = image.naturalWidth;
-            canvas.height = image.naturalHeight;
-            const context = canvas.getContext("2d");
-            requestAnimationFrame(_ => {
-                context.drawImage(image, 0, 0);
-                const dataUrl = canvas.toDataURL("image/png");
-                const a = document.createElement("a");
-                a.innerText = `Download ${changeExtension(file.name, "png")}`;
-                a.href = dataUrl;
-                a.download = changeExtension(file.name, "png");
-                linkOutput.appendChild(a);
-                canvases.appendChild(canvas);
-            });
-        }, {once: true});
-        image.src = URL.createObjectURL(file);
+        for (let i = 0; i < svgImport.files.length; i++) {
+            const file = svgImport.files[i];
+            const image = document.createElement("img");
+            image.addEventListener("load", _ => {
+                const canvas = document.createElement("canvas");
+                canvas.width = image.naturalWidth;
+                canvas.height = image.naturalHeight;
+                const context = canvas.getContext("2d");
+                requestAnimationFrame(_ => {
+                    context.drawImage(image, 0, 0);
+                    const dataUrl = canvas.toDataURL("image/png");
+                    const a = document.createElement("a");
+                    a.innerText = `Download ${changeExtension(file.name, "png")}`;
+                    a.href = dataUrl;
+                    a.download = changeExtension(file.name, "png");
+                    linkOutput.appendChild(a);
+                    canvases.appendChild(canvas);
+                });
+            }, { once: true });
+            image.src = URL.createObjectURL(file);
+        }
     });
 })();
